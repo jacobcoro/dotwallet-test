c78f80c9783c0a79ef636b147c047fbe621e4445
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-07-17 17:35:57 +0800

    heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
-app.use(express.static('src'));
-app.use(express.static('src/assets'));
+// app.use(express.static('src'));
+// app.use(express.static('src/assets'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/auth', async (req, res) => {
+  console.log('req, res', req, res);
+  const code = req.params.code;
+  const result = await axios.post(
+    'https://www.ddpurse.com/openapi/platform/access_token',
+    {
+      app_id: 'YOUR_APP_ID',
+      secret: 'YOUR_APP_SECRET',
+      code: code,
+    }
+  );
+  const accessToken = result.data.data.access_token;
+  if (accessToken) {
+    const userInfo = await axios.get(
+      'https://www.ddpurse.com/platform/openapi/get_user_info?access_token=' +
+        accessToken
+    );
+    res.redirect('/home/?name=' + userName);
+  }
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
