8923afd411fea1fa78d5967a97bf2c3dbfacf49a
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-07-20 18:06:51 +0800

    test successful!

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotwallet-test
- Trying out features of the DotWallet for Developers platform
+
+Change the example.env to .env and replace the values with your credentials from the DotWallet for Developers platform.
+
+Run with
+
+```bash
+npm run start
+```
--- src/index.js
+++ src/index.js
@@ -1,37 +1,56 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const dotenv = require('dotenv');
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static('src'));
+
+dotenv.config({ path: './.env' });
+const PORT = process.env.PORT || 3000;
 const YOUR_APP_SECRET = process.env.APP_SECRET;
 const YOUR_APP_ID = process.env.APP_ID;
+
+app.use(express.static('src'));
 
 app.get('/auth', async (req, res) => {
   // console.log('req, res', req, res);
   try {
-    const code = req.params.code;
+    const code = req.query.code;
+    console.log('==============got code==============\n', code);
+    const data = {
+      app_id: YOUR_APP_ID,
+      secret: YOUR_APP_SECRET,
+      code: code,
+    };
+    console.log('==============data==============\n', data);
+
     const result = await axios.post(
-      'https://www.ddpurse.com/openapi/platform/access_token',
-      {
-        app_id: YOUR_APP_ID,
-        secret: YOUR_APP_SECRET,
-        code: code,
-      }
+      'https://www.ddpurse.com/platform/openapi/access_token',
+      data
     );
+    console.log('==============access token result==============\n', result);
     const accessToken = result.data.data.access_token;
     if (accessToken) {
       const userInfo = await axios.get(
         'https://www.ddpurse.com/platform/openapi/get_user_info?access_token=' +
           accessToken
       );
-      res.redirect('/home/?name=' + userInfo.data.data.user_name);
+      console.log('==============user info result==============\n', userInfo);
+      res.redirect(
+        '/restricted-page/?name=' +
+          userInfo.data.data.user_name +
+          '&pic=' +
+          userInfo.data.data.user_avatar
+      );
     }
   } catch (err) {
-    console.log(err);
+    console.log('==============ERROR==============\n', err);
   }
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/restricted-page', async (req, res) => {
+  res.sendFile(path.join(__dirname + '/restricted-page.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
 );
